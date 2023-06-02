eec9db9ef7bfe380fbd480eb67a25db49edf1ae8
Tambua Evaristus <evaristustambua@gmail.com>
2023-06-02 18:08:27 +0100

    small mile stone

---
+++ pages/home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import React from "react";
+
+export default function home() {
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
