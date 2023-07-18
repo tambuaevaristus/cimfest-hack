44919ac58ef44792532c0c20fd4993e9927de927
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-18 16:26:52 +0100

    shaken

--- pages/file_upload.tsx
+++ pages/file_upload.tsx
@@ -284,7 +284,6 @@ export default function Create() {
                         className="form-radio h-4 w-4 text-blue-600"
                         value="plain"
                         name="printColor"
-                        // checked={value === 'false'}
                         onChange={(e) => setPrintType(e.target.value)}
                       />
                       <span className="ml-2">Plain</span>
