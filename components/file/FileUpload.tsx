1baf1b5042467ea53ff73168069faa5dd119af29
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-27 18:02:41 +0100

    small progress

--- components/file/FileUpload.tsx
+++ components/file/FileUpload.tsx
@@ -1,40 +1,54 @@
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { PDFDocument } from "pdf-lib";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
 
 export default function FileUpload() {
   const pageNavigationPluginInstance = pageNavigationPlugin();
-  const {CurrentPageLabel } = pageNavigationPluginInstance;
+  const { CurrentPageLabel } = pageNavigationPluginInstance;
+  const pageNumRef: any = useRef();
 
-  const [numPages, setNumPages] = useState(null);
+  const [numPages, setNumPages] = useState("");
 
   const [url, setUrl] = React.useState("");
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    files?.length > 0 && setUrl(URL.createObjectURL(files[0]));
+  const handleDivClick = () => {
+    const div = document.getElementById("my-span");
+    setNumPages(div?.innerHTML);
 
-    console.log("na the page this ===>", numPages);
+    console.log("we are in ")
+    console.log("page number ==>", parseInt(numPages));
   };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files: FileList = e.target?.files;
+    files.length > 0 && setUrl(URL.createObjectURL(files[0]));
+
+    handleDivClick()
+  };
+  // setNumPages(pageNumRef.current)
+
+ 
 
   return (
     <div>
-    <div  className="bg-gray-400 h-10 p-2" >
-    <input type="file" accept=".pdf" onChange={onChange} />
-      <CurrentPageLabel>
-                {(props: RenderCurrentPageLabelProps) => (
-                    <span>{`${props.currentPage + 1} of ${props.numberOfPages}`}</span>
-                )}
-            </CurrentPageLabel>
-    </div>
-      <div style={{ height: "750px" }}>
+      <div className="bg-gray-400 h-10 p-2  w- z-20">
+        <input type="file" accept=".pdf" onChange={onChange} />
+        <CurrentPageLabel>
+          {(props: any) => (
+            <span>
+              {`${props.currentPage + 1} of`}{" "}
+              <span
+                ref={pageNumRef}
+                id="my-span"
+                // onClick={handleDivClick}
+              >{`${props.numberOfPages}`}</span>
+            </span>
+          )}
+        </CurrentPageLabel>
+      </div>
+      <div>
         {url ? (
-          <div
-            style={{
-              border: "1px solid rgba(0, 0, 0, 0.3)",
-              height: "100%",
-            }}
-          >
+          <div>
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.min.js">
               <Viewer fileUrl={url} plugins={[pageNavigationPluginInstance]} />
             </Worker>
@@ -55,7 +69,6 @@ export default function FileUpload() {
           </div>
         )}
       </div>
-
     </div>
   );
 }
