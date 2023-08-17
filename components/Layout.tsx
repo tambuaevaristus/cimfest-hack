9ca5c881a72e2e227dd827faa6b3762a50c27374
Tambua Evaristus <evaristustambua@gmail.com>
2023-08-17 11:47:48 +0100

    amount change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,11 +4,16 @@ import Navbar from "./Navbar";
 import { useRouter } from "next/router";
 import { log } from "console";
 import { useSelector } from "react-redux";
+import AdminSidebar from "./AdminSidebar";
+import { useSession } from "next-auth/react";
 
 export default function Layout({ children }: any) {
   const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
   const router = useRouter();
-  
+  const session = useSession();
+  const user = session?.data?.user;
+  console.log(user?.role?.code);
+
   return (
     <div>
       <Navbar setIsNavOpen={setIsNavOpen} />
@@ -18,7 +23,11 @@ export default function Layout({ children }: any) {
           <></>
         ) : (
           <div className="lg:w-1/4">
-            <Sidebar isNavOpen={isNavOpen} />
+            {user?.role?.code == "user" ? (
+              <AdminSidebar isNavOpen={isNavOpen} />
+            ) : (
+              <Sidebar isNavOpen={isNavOpen} />
+            )}{" "}
           </div>
         )}
         <div className=" container mx-auto mt-[50px]">{children}</div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,128 +6,118 @@ import Navbar from "./Navbar";
 import Link from "next/link";
 // import userSlice from "@/slice/userSlice";
 import { useSelector } from "react-redux";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { RootState } from "@/store";
 
 export default function Sidebar({ isNavOpen }: any) {
   const userInfo = useSelector((state: RootState) => state.user);
   const role: string = "admin";
+
+  const session = useSession();
+  const user = session?.data?.user;
   return (
     <div>
-      <div className={isNavOpen ? "block lg:block" : "lg:block hidden"}>
-        <div className="fixed left-0 mt-[60px] z-20 p-5 h-screen overflow-y-auto bg-white md:block">
-          <div className="py-2 text-gray-500 dark:text-gray-400">
-            {/* <div className="flex justify-center items-center flex-col">
-              <div className="bg-gray-300 w-32 h-32 rounded-full overflow-hidden">
-                <img
-                  src={
-                    userInfo?.userData?.image
-                      ? userInfo?.userData?.image
-                      : "profileImage"
-                  }
-                  alt="Profile Image"
-                  className="w-full h-full object-cover"
-                />
+      <div>
+        <div className={isNavOpen ? "block lg:block" : "lg:block hidden"}>
+          <div className="max-w-[264px] bg-white h-screen pt-5 pb-4 flex-col justify-start items-start gap-5 inline-flex">
+            <div className="self-stretch h-8 px-4 flex-col justify-start items-start flex">
+              <div className="w-[143px] h-8 relative" />
+            </div>
+            <div className="h-[231px] flex-col justify-start items-center flex">
+              <div className="self-stretch h-px bg-gray-200" />
+              <div className="self-stretch px-4 pt-6 pb-2 justify-center items-center inline-flex">
+                <div className="flex-col justify-center items-center gap-3 inline-flex">
+                  <img
+                    className="w-[122px] h-[122px] rounded-[100px]"
+                    src={user?.image}
+                  />
+                  <div className="flex-col justify-center items-center flex">
+                    <div className="text-gray-700 text-base font-medium leading-normal">
+                      {user?.name}
+                    </div>
+                    <div className="text-zinc-500 text-sm font-medium leading-tight">
+                      676814364
+                    </div>
+                    <div className="text-zinc-500 text-sm font-medium leading-tight">
+                      {user?.email}
+                    </div>
+                  </div>
+                </div>
               </div>
-              <h2 className="text-xl font-bold mt-4">
-                {userInfo.userData?.name}
-              </h2>
-              <p className="text-gray-600 mt-2">{userInfo.userData?.email}</p>
-              <p className="text-gray-600">+237676814364</p>
-            </div> */}
-            <ul className="mt-6">
-              <li className="relative px-6 py-3">
-                <Link
-                  className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                  href="/"
-                >
-                  <BiHome size={20} />
-
-                  <span className="ml-4">Dashboard</span>
-                </Link>
-              </li>
-         
-              <li className="relative px-6 py-3">
-                <Link
-                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  href="cards.html"
-                >
-                  <BsCreditCard2Front size={20} />
-                  <span className="ml-4">Billing</span>
-                </Link>
-              </li>
-              {role == "user" ? (
-                <>
-                  <li className="relative px-6 py-3">
-                    <Link
-                      className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="/users"
-                    >
-                      <BsCreditCard2Front size={20} />
-                      <span className="ml-4">Users</span>
+            </div>
+            <div className="self-stretch grow shrink basis-0 px-6 flex-col justify-start items-start gap-1 flex">
+              <div className="w-full justify-start items-start inline-flex">
+                <div className="w-full self-stretch p-4 pr-8 hover:bg-violet-200 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <Link href="/">
+                      <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                        Dashboard
+                      </div>
                     </Link>
-                  </li>
-                  <li className="relative px-6 py-3">
-                    <Link
-                      className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="/commands"
-                    >
-                      <BsCreditCard2Front size={20} />
-                      <span className="ml-4">Commands</span>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full justify-start items-start inline-flex">
+                <div className="w-full self-stretch p-4 pr-8 hover:bg-violet-200 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <Link href="/profile">
+                      <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                        Profile
+                      </div>
                     </Link>
-                  </li>
-                </>
-              ) : (
-                <></>
-              )}
-              <li className="relative px-6 py-3">
-                <a
-                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  href="charts.html"
-                >
-                  <BiHelpCircle size={20} />
-                  <span className="ml-4">Help</span>
-                </a>
-              </li>
-              <li className="relative px-6 py-3">
-                <a
-                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  href="charts.html"
-                >
-                  <BiHelpCircle size={20} />
-                  <span className="ml-4">Setting</span>
-                </a>
-              </li>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="w-full justify-start items-start inline-flex">
+                <div className="w-full self-stretch p-4 pr-8 hover:bg-violet-200 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <Link href="/customers">
+                      <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                        Billing
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full justify-start items-start inline-flex">
+                <div className="w-full self-stretch p-4 pr-8 hover:bg-violet-200 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <Link href="/">
+                      <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                        Settings
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
 
-              <li className="relative px-6 py-3">
-                <Link
-                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  href="/profile"
-                >
-                  <BiUser size={20} />
-                  <span className="ml-4">Profile</span>
-                </Link>
-              </li>
-              <li className="relative px-6 py-3">
-                {/* <Link
-                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                  href="signin"
-                > */}
-                <BiLogOut size={20} />
-                <span className="ml-4" onClick={() => signOut()}>
-                  Logout
-                </span>
-                {/* </Link> */}
-              </li>
-            </ul>
-            {/* <div className="px-6 my-6">
-            <button className="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-              Create account
-              <span className="ml-2" aria-hidden="true">
-                +
-              </span>
-            </button>
-          </div> */}
+              <div className="justify-start items-start inline-flex">
+                <div className="self-stretch p-4 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                      Help
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="justify-start items-start inline-flex">
+                <div className="self-stretch p-4 rounded-md justify-between items-center gap-[105px] flex">
+                  <div className="grow shrink basis-0 h-6 justify-start items-center gap-3 flex">
+                    <div className="w-6 h-6 relative" />
+                    <button onClick={() => signOut()}>
+                      <div className="grow shrink basis-0 text-zinc-500 text-base font-normal leading-tight">
+                        Logout
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
