9940658b16ac5de758d8cab93e023abaf3a164c0
Tambua Evaristus <evaristustambua@gmail.com>
2023-11-13 04:34:34 +0100

    initial cimfest commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import Body from "@/components/Body";
-import Dashboard from "@/components/Dashboard";
-import Navbar from "@/components/Navbar";
-import OrderList from "@/components/FileList";
-import Sidebar from "@/components/Sidebar";
-import PDFViewer from "@/components/file/PDFViewer";
-import Pdf from "@/components/file/PDFViewer";
-import Link from "next/link";
 import React from "react";
 
 const index = () => {
   return (
     <div>
-      <div className="py-5  float-right container">
-        <Dashboard />
-        <OrderList />
-
-        <PDFViewer />
-      </div>{" "}
+      <div className="py-5  float-right container">heloo</div>{" "}
     </div>
   );
 };
