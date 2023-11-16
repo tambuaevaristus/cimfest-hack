5ad0cbd8bfc8056b4acb53aa356da9114b1d87da
Tambua Evaristus <evaristustambua@gmail.com>
2023-11-16 10:58:03 +0100

    jjjj

--- components/general/SideBar.tsx
+++ components/general/SideBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { CiHome, CiMoneyBill, CiMoneyCheck1 } from "react-icons/ci";
 import { ImBooks, ImHome, ImLibrary, ImSearch } from "react-icons/im";
@@ -49,10 +50,12 @@ export default function SideBar() {
         </div>
         <hr className="border-gray-200 mx-6 mb-3" />
         <div className="mb-3">
-          <div className="px-6 hover:bg-white/5 py-2 gap-3 cursor-pointer flex items-center text-sm text-white opacity-50 hover:opacity-100">
+         <Link href="/payment">
+         <div className="px-6 hover:bg-white/5 py-2 gap-3 cursor-pointer flex items-center text-sm text-white opacity-50 hover:opacity-100">
             <CiMoneyCheck1 className="h-9 w-9"  />
             Pricing
           </div>
+         </Link>
         </div>
         <div>
           {/* <div className="px-6 py-2 flex items-center text-sm text-white text-gray-100 hover:text-white">
